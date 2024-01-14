aad1ed3bf3344f3a21e1d029b92fca3c3218d3a7
Matarwy <hassanmatarwy@gmail.com>
2024-01-15 00:02:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,12 +23,28 @@ const styles = {
 
 function App() {
   const [wordsData, setWordsData] = useState(null);
+  const [selectedVoice, setSelectedVoice] = useState(null);
 
   useEffect(() => {  
       setWordsData(allwords);
-      
+      // Fetch available voices when the component mounts
+      const voices = speechSynthesis.getVoices();
+      // Set the initial selected voice (you can choose a default or let the user decide)
+      setSelectedVoice(voices[0]);
   }, []);
   
+
+  const handleVoiceChange = (event) => {
+    const selectedVoiceName = event.target.value;
+    const voices = speechSynthesis.getVoices();
+    const newSelectedVoice = voices.find((voice) => voice.name === selectedVoiceName);
+    setSelectedVoice(newSelectedVoice);
+  };
+
+  const wordListProps = {
+    selectedVoice,
+    onVoiceChange: handleVoiceChange,
+  };
 
 
   if (!wordsData) {
@@ -37,10 +53,19 @@ function App() {
 
   return (
     <div className="App" style={styles.app}>
-      <header  style={styles.header}>Wafi Translation</header >
+      <h1 style={styles.header}>Wafi Translation</h1>
+      <header style={styles.header}>
+        <label htmlFor="voices">Select a voice:</label>
+        <select id="voices" onChange={handleVoiceChange} value={selectedVoice ? selectedVoice.name : ''}>
+          {speechSynthesis.getVoices().map((voice) => (
+            <option key={voice.name} value={voice.name}>
+              {voice.name}
+            </option>
+          ))}
+        </select>
+      </header>
       {wordsData.map((category, index) => (
-        // console.log(category.words)
-        <WordList key={index} category={category.name} words={category.words} />
+        <WordList key={index} category={category.name} words={category.words} selectedVoice={selectedVoice} />
       ))}
     </div>
   );
